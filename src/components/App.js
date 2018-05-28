9f41a5e5d1df37c7d865192c22c921a02fc0e32f
aefnor <aefnor@yahoo.com>
2018-05-27 17:00:07 -0700

    added react-strap and bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,11 @@ import React from 'react'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
+import { Button } from 'reactstrap';
 
 const App = () => (
     <div>
+        <Button color="danger">Danger!</Button>
         <AddTodo />
         <VisibleTodoList />
         <Footer />
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
 import App from './components/App'
 import logger from 'redux-logger'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const store = createStore(rootReducer, applyMiddleware(logger))
 
